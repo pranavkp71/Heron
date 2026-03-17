ee0c971afe2381a39500a994955cbf65aa9aca6d
pranavkp71 <pranavkp170@gmail.com>
2026-03-17 14:34:01 +0530

    Add API key generator utility and project service for project creation

---
+++ heron-server/app/services/project_service.py
@@ -0,0 +1,24 @@
+from app.database import get_connection
+from app.utils.api_key import generate_api_key
+
+
+def create_project(name: str):
+    
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    api_key = generate_api_key()
+
+    cursor.execute(
+        """
+        INSERT INTO projects (name, api_key)
+        VALUES (%, %)
+        RETURNING id, api_key
+        """,
+        (name, api_key)
+    )
+
+    project = cursor.fetchone()
+    conn.commit()
+
+    return project
