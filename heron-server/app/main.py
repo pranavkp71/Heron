9d4ffb63b7094b19679063ead3a9d8861a8ff67c
pranavkp71 <pranavkp170@gmail.com>
2026-03-20 22:00:27 +0530

    Register incident routes in main app

--- heron-server/app/main.py
+++ heron-server/app/main.py
@@ -3,11 +3,13 @@ from app.routes.events import router as event_router
 import threading
 from app.worker import run_worker
 from app.routes import projects
+from app.routes import incidents
 
 app = FastAPI()
 
 app.include_router(event_router)
 app.include_router(projects.router)
+app.include_router(incidents.router)
 
 @app.get("/")
 def root():
