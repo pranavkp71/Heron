d91e6c0a2425e86a09198545c554fd89a9281820
pranavkp71 <pranavkp170@gmail.com>
2026-04-17 21:36:46 +0530

    Add supabase

--- heron-server/app/database.py
+++ heron-server/app/database.py
@@ -4,17 +4,10 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-conn = psycopg2.connect(
-    dbname = DB_NAME,
-    user = DB_USER,
-    password = DB_PASSWORD,
-    host = DB_HOST
-)
+# Use the DATABASE_URL directly for connection
+conn = psycopg2.connect(DATABASE_URL)
 
 def get_connection():
     return conn
